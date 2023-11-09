d941edca79649131b032c0ad91a46adec52ebd01
TJ Draper <tj@buzzingpixel.com>
2023-11-09 12:41:21 -0600

    Add pnpm cli commands

---
+++ cli/src/commands/dev/pnpm-frontend-build.ts
@@ -0,0 +1,16 @@
+import { Command } from '@oclif/core';
+import { execSync } from 'node:child_process';
+import * as fs from 'fs-extra';
+
+export default class PnpmFrontendBuild extends Command {
+    // eslint-disable-next-line class-methods-use-this
+    public async run (): Promise<void> {
+        const rootDir = fs.realpathSync(`${this.config.root}/../`);
+
+        execSync(`
+            cd ${rootDir};
+            pnpm build-css;
+            pnpm build-js;
+        `, { stdio: 'inherit' });
+    }
+}
